11946a95147e7dfb9fc0eec5956402028e9a7236
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-06-11 13:01:03 +0900

    修正

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -14,7 +14,14 @@ useState("products", () => [
   },
   { id: 1, imgUrl: "/ume.jpg", name: "Daigo", price: 15000 },
   { id: 2, imgUrl: "/yumi.webp", name: "Yumi", price: 30000 },
-  { id: 3, imgUrl: "/hori.png", name: "Shingo", price: 30000 },
+  {
+    id: 3,
+    name: "Shingo",
+    price: 12000,
+    imgUrl: "/hori.png",
+    imgUrl_2: "/hori_3.avif",
+    desc: "48000です",
+  },
 ]);
 </script>
 
